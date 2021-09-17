86725001999b69b978cf79a3324a536fa4a5b186
Fernanda Andrade <fernandaao@gmail.com>
2021-09-16 21:49:23 -0300

    new trail

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,9 +1,27 @@
 class TrailsController < ApplicationController 
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @trails = Trail.all
+  end
 
-  def new; end
+  def new 
+    @trail = Trail.new
+  end
 
-  def create; end
+  def create
+    @trail = Trail.new(trail_params)
+
+    if @trail.save
+      redirect_to trails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def trail_params 
+      params.require(:trail).permit(:name)
+    end
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,11 @@
 class Trail < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true 
+
+  before_validation :sanitize_name!
+
+  private
+    def sanitize_name!
+      self.name = self.name.to_s.downcase
+    end 
 end
